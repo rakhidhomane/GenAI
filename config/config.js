d110c4d1c33dc9962231a302080349c67f41c321
rakhidhomane <97904504+rakhidhomane@users.noreply.github.com>
2026-04-13 11:34:14 +0530

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+    host: "localhost",
+    user: "root",
+    password: "root123", // Hardcoded credentials
+    database: "users_db"
+  },
+  jwtSecret: "mysecretkey" // Hardcoded secret
+};
